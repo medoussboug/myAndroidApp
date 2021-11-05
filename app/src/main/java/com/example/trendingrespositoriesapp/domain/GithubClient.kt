4ce4bfec636d455cb0092831553733dcae378f39
medoussboug <mohamedoussama.bougueddach@gmail.com>
2021-11-05 15:54:40 +0100

    Update GithubClient Interface

---
+++ app/src/main/java/com/example/trendingrespositoriesapp/domain/GithubClient.kt
@@ -0,0 +1,30 @@
+package com.example.trendingrespositoriesapp.domain
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import java.text.SimpleDateFormat
+import java.util.*
+
+private val DATE = SimpleDateFormat("YYYY-MM-DD", Locale.getDefault()).toString()
+private val BASE_URL_TRENDING_REPOSITORIES =
+    "https://api.github.com/search/repositories?q=created:>" + DATE
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL_TRENDING_REPOSITORIES)
+    .build()
+
+interface GithubClient {
+    @GET("&sort=stars&order=desc")
+    fun getMostStaredRepositories(): TrendingRepositoriesDTO
+}
+
+object GithubService {
+    val githubClient: GithubClient by lazy { retrofit.create(GithubClient::class.java) }
+}
